b77d99b1ad14746ae7a9df6d6958e0ff3b50cf64
withmedical <withm1905@withmedical.com>
2026-07-27 19:12:48 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="with-welfare-v28-0-9-production";
+const CACHE_NAME="with-welfare-v28-0-9-2-production";
 const ASSETS=["/","/index.html","/app.js?v=28_0_9_production","/style.css?v=28_0_9","/manifest.webmanifest?v=28_0_9","/icon-192.png","/icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
